d99c5f7dfffd8709fb7a523ecb42949a617026f1
Andras Salamon <andras.salamon@melda.info>
2016-04-09 22:56:59 +0200

    separating solarmiddle.sh

--- README.md
+++ README.md
@@ -11,3 +11,10 @@ usage: solarwind.sh inputfile
 |-----|------|
 |[![input](../gh-pages/sample_input_300.jpg)](../gh-pages/sample_input_1000.jpg)|[![output](../gh-pages/sample_output_300.jpg)](../gh-pages/sample_output_1000.jpg)|
 
+# solarmiddle
+
+Automatic symmetric positioning of total solar eclipse corona images
+
+```
+usage: solarmiddle.sh inputfile
+```
--- solarwind.sh
+++ solarwind.sh
@@ -70,33 +70,11 @@ mkdir "$SWTMPDIR" || error "CANNOT CREATE TEMPORARY FILE DIRECTORY"
 inputbase=${inputfile##*/}
 inputbase=${inputbase%.*}
 
-findmiddle() {
-    convert ${inputfile} -colorspace gray -auto-level -modulate 5000 +dither -colors 2 -contrast-stretch 0 -morphology Open Disk:${opendisk} -morphology Close Disk:${closedisk} -bordercolor black -border 10x10 -fill white -floodfill +0+0 black ${SWTMPDIR}/${inputbase}_mask.png
-    trimbox=$(convert ${SWTMPDIR}/${inputbase}_mask.png -trim -format "%X %Y %@" info:);
-    bsize=$(echo $trimbox | cut -f 3 -d ' ' | cut -f 1 -d '+')
-    bsizex=$(echo $bsize | cut -f 1 -d 'x')
-    bsizey=$(echo $bsize | cut -f 2 -d 'x')
-    bx=$(echo $trimbox | cut -f 1 -d ' ' | cut -f 2 -d '+')
-    bx2=$(echo $trimbox | cut -f 3 -d ' ' | cut -f 2 -d '+')
-    by=$(echo $trimbox | cut -f 2 -d ' ' | cut -f 2 -d '+')
-    by2=$(echo $trimbox | cut -f 3 -d ' ' | cut -f 3 -d '+')
-    bx=$(($bx+$bx2-10))
-    by=$(($by+$by2-10))
-#    convert $1.png -fill none -stroke red -strokewidth 1 -draw "rectangle ${bx},${by} $(($bx+$bsizex)),$(($by+$bsizey))" $1_rect.png
-    midx=$((${bx}+${bsizex}/2))
-    midy=$((${by}+${bsizey}/2))
-    iwidth=$(identify -ping -format "%w" ${inputfile})
-    iheight=$(identify -ping -format "%h" ${inputfile})
-    fsizex=$((${midx} > ${iwidth}-${midx} ? 2*(${iwidth}-${midx}) : 2*${midx}))
-    fsizey=$((${midy} > ${iheight}-${midy} ? 2*(${iheight}-${midy}) : 2*${midy}))
-    TOPX=$(($midx-$fsizex/2))
-    TOPY=$(($midy-$fsizey/2))
-    convert ${inputfile} -crop ${fsizex}x${fsizey}+${TOPX}+${TOPY} +repage ${SWTMPDIR}/${inputbase}_cuta.png
-}
+MYDIR="$(dirname "$(realpath "$0")")"
 
-findmiddle $1
+${MYDIR}/solarmiddle.sh --opendisk=${opendisk} --closedisk=${closedisk} $1
 
-convert ${SWTMPDIR}/${inputbase}_cuta.png \
+convert ${inputbase}_cuta.png \
     \( -size ${fsizex}x${fsizey} radial-gradient:black-white -gamma 0.3 \) \
     \( -clone 0 -colorspace gray \) \
     \( -clone 0 -clone 1 -compose Multiply -composite -normalize \) \
@@ -116,6 +94,3 @@ convert ${SWTMPDIR}/${inputbase}_cuta.png \
     \( +clone -clone 10 -compose Plus -composite \) \
     -delete 0--2 \
 ${inputbase}_swa.png
-
-
-
